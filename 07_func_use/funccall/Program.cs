c69b297acb85cca496951319da36e83670801329
Daimien Karsten <36749@ma-web.nl>
2025-10-30 09:59:59 +0100

    inleveren

---
+++ 07_func_use/funccall/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace funccall;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.Write("....X....");
+            Console.SetCursorPosition(4, 0);
+            Console.Write('?');
+
+            Console.SetCursorPosition(0, 1);
+            Console.Write("deze regel is normaal ");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("deze tekst is groen");
+            Console.ResetColor();
+        }
+    }
