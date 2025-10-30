4a90ac6356e44223788d70296b6f974fb0253e6b
Daimien Karsten <36749@ma-web.nl>
2025-10-30 10:23:58 +0100

    inleveren

--- 07_func_use/math/Program.cs
+++ 07_func_use/math/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+       double kleiner = 9;
+        double groter = 19;
+        double welkeIsKleiner = Math.Min(kleiner, kleiner);
+        Console.WriteLine($"{welkeIsKleiner:0.##} is kleinste van {kleiner:0.##} en {groter:0.##}");
+
+        double Groter = 14;
+        double Kleiner = 9;
+        double WelkeIsGroter = Math.Min(Groter, Groter);
+        Console.WriteLine($"{WelkeIsGroter:0.##} is Grootste van {Kleiner:0.##} en {Groter:0.##}");
+
+        double value = 16;
+        double clamped = Math.Clamp(value, 20, 30);
+        double sqrt = Math.Sqrt(value);
+        Console.WriteLine($"het clamp resultaat van {value} met min 20 en max 30 is de {clamped} de Sqrt van {value} is de {sqrt:0}");
     }
 }
