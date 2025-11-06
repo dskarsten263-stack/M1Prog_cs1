39fa73e6ef5e0cc8e6e94b7120329c39ac46e880
Daimien Karsten <36749@ma-web.nl>
2025-11-06 09:28:42 +0100

    inleveren

--- 07_func_use/forfunc/Program.cs
+++ 07_func_use/forfunc/Program.cs
@@ -2,8 +2,26 @@
 
 class Program
 {
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        
+        int value1 = 2;
+        for (int power = 2; power <= 2; power++)
+            Console.WriteLine($"{value1} keer zichzelf {Math.Pow(value1, power):N0} ");
+            
+        
+        int value2 = 5;
+        for (int power = 2; power <= 2; power++)
+            Console.WriteLine($"{value2} keer zichzelf {Math.Pow(value2, power):N0} ");
+        
+        int value3 = 78;
+        for (int power = 2; power <= 2; power++)
+            Console.WriteLine($"{value3} keer zichzelf {Math.Pow(value3, power):N0} ");
+   
+        int value4 = 98;
+        for (int power = 2; power <= 2; power++)
+            Console.WriteLine($"{value4} keer zichzelf {Math.Pow(value4, power):N0}");
+   
     }
 }
